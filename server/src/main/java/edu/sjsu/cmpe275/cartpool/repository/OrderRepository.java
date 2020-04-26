987d64cf968ca4fbbca23f43ea20ffa95ea80611
Akshit Ahuja <akshit.ahuja@sjsu.edu>
2020-04-26 14:33:32 -0700

    Order repository

--- server/src/main/java/edu/sjsu/cmpe275/cartpool/repository/OrderRepository.java
+++ server/src/main/java/edu/sjsu/cmpe275/cartpool/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package edu.sjsu.cmpe275.cartpool.repository;
 
 import edu.sjsu.cmpe275.cartpool.pojos.Orders;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface OrderRepository<T> extends CrudRepository<Orders, Long> {
+	Optional<Orders> findById(Long id);
 }
