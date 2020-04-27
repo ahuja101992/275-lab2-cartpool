8e491be4af3de949fe67c1ce36b2465d72c8478a
Akshit Ahuja <akshit.ahuja@sjsu.edu>
2020-04-27 10:21:48 -0700

    getOrdersForPickUp repository

--- server/src/main/java/edu/sjsu/cmpe275/cartpool/service/OrderService.java
+++ server/src/main/java/edu/sjsu/cmpe275/cartpool/service/OrderService.java
@@ -11,4 +11,5 @@ public interface OrderService {
     Orders getOrderDetails(long id);
 
     List<Orders> getOrdersByOwnerId(long id);
+    List<Orders> getOrdersForPickUp(long id, long storeId);
 }
