b425c84874531f538a7ce1e84d57b22a460853aa
vijay2502 <vijay.ghanshani@sjsu.edu>
2020-04-28 03:08:33 -0700

    Update SearchPool.js

--- client/src/components/SearchPool/SearchPool.js
+++ client/src/components/SearchPool/SearchPool.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-
-
-
+import { HOSTNAME } from "../../constants/appConstants";
+import axios from 'axios';
 
 class SearchPool extends Component {
-    state = {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchParam: "",
+            pools: []
+        };
+        this.renderSelectedComponent = this.renderSelectedComponent.bind(this);
+    }
+
+    submitHandler = () => {
+        let searchParam = this.state.searchParam;
+        axios.defaults.withCredential = true;
+        axios.get(`http://${HOSTNAME}:8080/pool/search/${searchParam}`)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    };
+
+    changeHandeler = (e) => {
+        this.setState({
+            searchParam: e.target.value
+        });
+    };
+
     render() {
         return (
             <React.Fragment>
-
+                <div><input type="text" className="search-box" value={this.state.searchParam} onChange={this.changeHandeler}
+                    placeholder="Pizza, sushi, ...." /></div>
 
             </React.Fragment>
         );
