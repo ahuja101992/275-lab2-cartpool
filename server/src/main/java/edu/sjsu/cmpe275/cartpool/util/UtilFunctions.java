c12aec89fd5f29871d655472e27f9e87f35bc9b8
vijay2502 <vijay.ghanshani@sjsu.edu>
2020-04-28 01:23:58 -0700

    adding validateZip method

--- server/src/main/java/edu/sjsu/cmpe275/cartpool/util/UtilFunctions.java
+++ server/src/main/java/edu/sjsu/cmpe275/cartpool/util/UtilFunctions.java
@@ -1,8 +1,19 @@
 package edu.sjsu.cmpe275.cartpool.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UtilFunctions {
     public final static boolean isAdmin(String email) {
         String domainName = email.split("@")[1];
         return "sjsu.edu".equals(domainName);
     }
+
+    public final static boolean validateZipcode(String zip) {
+        String regex = "^[0-9]{5}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(zip);
+        return matcher.matches();
+    }
+
 }
