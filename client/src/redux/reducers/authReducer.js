5cbf54ee2c85a6b497577a87ac80eb31e12c19c4
Vini Aswal <vijendersingh.aswal@sjsu.edu>
2020-04-28 14:34:59 -0700

    Fixed login pasge styling

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -17,10 +17,11 @@ export default function authReducer(state = initialState, action) {
 
     if (action.type === SIGN_IN) {
         localStorage.setItem('id', action.payload.id);
+        localStorage.setItem('screenname', action.payload.id);
+        localStorage.setItem('email', action.payload.id);
 
         return Object.assign({}, state, {
             signinSuccess: action.payload.id !== null ? true : false,
-            signinMessage: ""
         });
     } else if (action.type === SIGN_IN_ERROR) {
         return Object.assign({}, state, {
