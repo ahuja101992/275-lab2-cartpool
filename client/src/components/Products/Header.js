2e76ffab7432d798642e09a61731b61df7b658e4
Akshit Ahuja <akshit.ahuja@sjsu.edu>
2020-05-07 12:48:11 -0700

    new Item UI button issue

--- client/src/components/Products/Header.js
+++ client/src/components/Products/Header.js
@@ -4,8 +4,9 @@ import Counter from "./Counter";
 import EmptyCart from "./EmptyCart";
 import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
 import { findDOMNode } from "react-dom";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import AddItem from "../AdminPages/AddItem";
+import "./css/item.css";
 
 class Header extends Component {
   constructor(props) {
@@ -15,64 +16,63 @@ class Header extends Component {
       showCart: false,
       cart: this.props.cartItems,
       mobileSearch: false,
-      modal:false,
-      value:true
+      modal: false,
+      value: true,
     };
-      this.increment = this.increment.bind(this);
-      this.decrement = this.decrement.bind(this);
-      this.handleUpdate= this.handleUpdate.bind(this);
-      this.cancelEdit = this.cancelEdit.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.cancelEdit = this.cancelEdit.bind(this);
   }
-
 
   increment(product) {
     this.setState({
-      value:true
-    })
-    console.log("product increment",product.qty)
+      value: true,
+    });
+    console.log("product increment", product.qty);
     this.props.addOne(product);
   }
 
-  cancelEdit(){
+  cancelEdit() {
     this.setState({
-      modal : false
-    })
- };
+      modal: false,
+    });
+  }
 
   decrement(product) {
-    if(product.qty>0){
-    console.log("product delete ",product)
-   this.props.deleteOne(product);
-    }else{
+    if (product.qty > 0) {
+      console.log("product delete ", product);
+      this.props.deleteOne(product);
+    } else {
       this.setState({
-        value:false
-      })
+        value: false,
+      });
     }
   }
 
   handleCart(e) {
     e.preventDefault();
     this.setState({
-      showCart: !this.state.showCart
+      showCart: !this.state.showCart,
     });
   }
 
   handleSubmit(e) {
     const searchStoreId = findDOMNode(this.refs.searchStoreId);
-    console.log("dom::",searchStoreId);
+    console.log("dom::", searchStoreId);
     e.preventDefault();
   }
 
-  handleUpdate(e){
+  handleUpdate(e) {
     this.setState({
-      modal : true
-    })
+      modal: true,
+    });
   }
 
   handleMobileSearch(e) {
     e.preventDefault();
     this.setState({
-      mobileSearch: true
+      mobileSearch: true,
     });
   }
 
@@ -80,9 +80,9 @@ class Header extends Component {
     e.preventDefault();
     this.setState(
       {
-        mobileSearch: false
+        mobileSearch: false,
       },
-      function() {
+      function () {
         this.refs.searchBox.value = "";
         this.props.handleMobileSearch();
       }
@@ -116,40 +116,52 @@ class Header extends Component {
   }
   render() {
     let cartItems;
-    let searchBySku=<input
-    type="search"
-    ref="searchBoxSku"
-    placeholder="Search By Sku"
-    className="search-keyword"
-    onChange={this.props.handleSearchBySku}
-  />
-let searchByStoreId=<input
-    type="search"
-    ref="searchStoreId"
-    placeholder="Search By StoreId"
-    className="search-keyword"
-    onChange={this.props.handleSearchByStoreId}
-  />
+    let searchBySku = (
+      <input
+        type="search"
+        ref="searchBoxSku"
+        placeholder="Search By Sku"
+        className="search-keyword"
+        onChange={this.props.handleSearchBySku}
+      />
+    );
+    let searchByStoreId = (
+      <input
+        type="search"
+        ref="searchStoreId"
+        placeholder="Search By StoreId"
+        className="search-keyword"
+        onChange={this.props.handleSearchByStoreId}
+      />
+    );
 
-let adItem=<button 
-variant="primary"
-  type="button"
-  onClick={this.handleUpdate.bind(this
-  )}>NEW ITEM
-</button> 
+    let adItem = (
+      <button
+        class="new-iten-btn"
+        variant="primary"
+        type="button"
+        onClick={this.handleUpdate.bind(this)}
+      >
+        New Item
+      </button>
+    );
 
- let create=<AddItem  
- cancelEdit={this.cancelEdit}
- getAll={this.props.getAll}
- {...this.state}></AddItem>
- let showModal = !this.state.modal ? "" : create;
-  let showSearchBySku = localStorage.getItem('type') === "admin" ? searchBySku : "";
-  let showSearchByStoreId = localStorage.getItem('type') === "admin" ? searchByStoreId : "";
-  let showAddItem = localStorage.getItem('type') === "admin" ? adItem : "";
+    let create = (
+      <AddItem
+        cancelEdit={this.cancelEdit}
+        getAll={this.props.getAll}
+        {...this.state}
+      ></AddItem>
+    );
+    let showModal = !this.state.modal ? "" : create;
+    let showSearchBySku =
+      localStorage.getItem("type") === "admin" ? searchBySku : "";
+    let showSearchByStoreId =
+      localStorage.getItem("type") === "admin" ? searchByStoreId : "";
+    let showAddItem = localStorage.getItem("type") === "admin" ? adItem : "";
 
-    cartItems = this.state.cart.map(product => {
+    cartItems = this.state.cart.map((product) => {
       return (
-
         <li className="cart-item" key={product.name}>
           <img className="product-image" src={product.image} />
           <div className="product-info">
@@ -158,27 +170,20 @@ variant="primary"
           </div>
           <div className="product-total">
             <p className="quantity">
-            <button
-            type="button"
-            onClick={this.increment.bind(
-              this,
-             product
-            )}
-          >
-            <b> + </b>
-          </button>
-
-        {product.qty} {product.qty > 1 ? "Nos." : "No."}{" "}
-        <button
-        disabled={!this.state.value}
-            type="button"
-            onClick={this.decrement.bind(
-              this,
-             product
-            )}
-          >
-            <b> - </b>
-          </button>
+              <button
+                type="button"
+                onClick={this.increment.bind(this, product)}
+              >
+                <b> + </b>
+              </button>
+              {product.qty} {product.qty > 1 ? "Nos." : "No."}{" "}
+              <button
+                disabled={!this.state.value}
+                type="button"
+                onClick={this.decrement.bind(this, product)}
+              >
+                <b> - </b>
+              </button>
             </p>
             <p className="amount">{product.qty * product.price}</p>
           </div>
@@ -208,83 +213,89 @@ variant="primary"
         </CSSTransitionGroup>
       );
     }
-    let bag=<div className="cart">
-  <div className="cart-info">
-    <table>
-      <tbody>
-        <tr>
-          <td>No. of items</td>
-          <td>:</td>
-          <td>
-            <strong>{this.props.totalItems}</strong>
-          </td>
-        </tr>
-        <tr>
-          <td>Sub Total</td>
-          <td>:</td>
-          <td>
-            <strong>{this.props.total}</strong>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <a
-    className="cart-icon"
-    href="#"
-    onClick={this.handleCart.bind(this)}
-    ref="cartButton"
-  >
-    <img
-      className={this.props.cartBounce ? "tada" : " "}
-      src="https://res.cloudinary.com/sivadass/image/upload/v1493548928/icons/bag.png"
-      alt="Cart"
-    />
-    {this.props.totalItems ? (
-      <span className="cart-count">{this.props.totalItems}</span>
-    ) : (
-      ""
-    )}
-  </a>
-  <div
-    className={
-      this.state.showCart ? "cart-preview active" : "cart-preview"
-    }
-    ref="cartPreview"
-  >
-    <CartScrollBar>{view}</CartScrollBar>
-    <div className="action-block">
-    <Link to={{ pathname: '/homePooler/checkout/', 
-      state: {
-          id: this.props.storeid,
-          store: this.props.storeid,
-          pool: "",
-          qty: this.props.totalItems,
-          items :this.props.cart,
-          price: this.props.totalAmount,
-          finalPrice: "",
-          available: true,
-          forDelivery: true,
-          status: "Placed",
-          orderOwner: "",
-          deliveryBy: null
-        }
-      }}><button
-        type="button"
-        className={this.state.cart.length > 0 ? " " : "disabled"}>
-        PROCEED TO CHECKOUT
-      </button></Link>
-    </div>
-  </div>
-</div>
-  let showCart = localStorage.getItem('type') === "pooler" ? bag : "";
+    let bag = (
+      <div className="cart">
+        <div className="cart-info">
+          <table>
+            <tbody>
+              <tr>
+                <td>No. of items</td>
+                <td>:</td>
+                <td>
+                  <strong>{this.props.totalItems}</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>Sub Total</td>
+                <td>:</td>
+                <td>
+                  <strong>{this.props.total}</strong>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <a
+          className="cart-icon"
+          href="#"
+          onClick={this.handleCart.bind(this)}
+          ref="cartButton"
+        >
+          <img
+            className={this.props.cartBounce ? "tada" : " "}
+            src="https://res.cloudinary.com/sivadass/image/upload/v1493548928/icons/bag.png"
+            alt="Cart"
+          />
+          {this.props.totalItems ? (
+            <span className="cart-count">{this.props.totalItems}</span>
+          ) : (
+            ""
+          )}
+        </a>
+        <div
+          className={
+            this.state.showCart ? "cart-preview active" : "cart-preview"
+          }
+          ref="cartPreview"
+        >
+          <CartScrollBar>{view}</CartScrollBar>
+          <div className="action-block">
+            <Link
+              to={{
+                pathname: "/homePooler/checkout/",
+                state: {
+                  id: this.props.storeid,
+                  store: this.props.storeid,
+                  pool: "",
+                  qty: this.props.totalItems,
+                  items: this.props.cart,
+                  price: this.props.totalAmount,
+                  finalPrice: "",
+                  available: true,
+                  forDelivery: true,
+                  status: "Placed",
+                  orderOwner: "",
+                  deliveryBy: null,
+                },
+              }}
+            >
+              <button
+                type="button"
+                className={this.state.cart.length > 0 ? " " : "disabled"}
+              >
+                PROCEED TO CHECKOUT
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+    let showCart = localStorage.getItem("type") === "pooler" ? bag : "";
     return (
       <header>
         {showModal}
         <div className="container">
-          <div className="brand">
-             {showAddItem}
-          </div>
+          <div className="brand">{showAddItem}</div>
 
           <div className="search">
             <a
@@ -331,8 +342,7 @@ variant="primary"
               />
             </form>
           </div>
-           {showCart}
-
+          {showCart}
         </div>
       </header>
     );
