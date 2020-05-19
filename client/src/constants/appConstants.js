214a8a7e9c355675cf09bbbaa566ab21bc5d63a1
Vini Aswal <vijendersingh.aswal@sjsu.edu>
2020-05-19 11:47:43 -0700

    Added UI changes

--- client/src/constants/appConstants.js
+++ client/src/constants/appConstants.js
@@ -1,8 +1,8 @@
-//export const HOSTNAME = "ec2-34-214-17-38.us-west-2.compute.amazonaws.com";
-export const HOSTNAME = "localhost";
+export const HOSTNAME = "ec2-52-43-101-58.us-west-2.compute.amazonaws.com";
+//export const HOSTNAME = "localhost";
 export const PLACED = "Placed";
 export const PICKED_UP_BY_SELF = "Picked-up by self";
 export const PICKED_UP = "Picked-up";
 export const DELIVERED = "Delivered";
-export const DELIVERED_NOT_RECEIVED = "Delivered (but not received)";
+export const DELIVERED_NOT_RECEIVED = "Delivered but not received";
 export const CANCELED = "Canceled";
